a59e3885423ae097afc99d0f24585c509bc2eb69
tenseite <bogdahnnikonov@yandex.ru>
2025-06-26 09:52:56 +0300

    Add files via upload

---
+++ 10/WinFormsApp_ImpUserInterface/WinFormsApp_ImpUserInterface/Class_Task3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp_ImpUserInterface
+{
+    internal class Class_Task3
+    {
+    }
+}
